e881f97c673dabb484c7e7fa4cc1f575c279660f
Marc-Andre Lafortune <github@marc-andre.ca>
2020-11-16 21:39:29 -0500

    Initial commit

---
+++ lib/ractor/supervisor.rb
@@ -0,0 +1,8 @@
+require "ractor/supervisor/version"
+
+module Ractor
+  module Supervisor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ractor/supervisor/version.rb
@@ -0,0 +1,5 @@
+module Ractor
+  module Supervisor
+    VERSION = "0.1.0"
+  end
+end
